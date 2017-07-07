7dc2237bcbd8ebe4a84fd6d0b1afc88ebcb13c8c
Cliff Rodgers <kliph@users.noreply.github.com>
2017-07-06 21:37:51 -0400

    Cards can be selected
    - Want to allow them to be inserted into slots eventually
    
    - Need to keep track of selected card in the state

--- src/storyleaves/cards.cljs
+++ src/storyleaves/cards.cljs
@@ -1,15 +1,30 @@
-(ns storyleaves.cards)
+(ns storyleaves.cards
+  (:require [reagent.core :as r]
+            [storyleaves.state :as state]))
 
-(defn card-border [& children]
+(defn card-border [click-fn selected-atom & children]
   [:div.card-border
-   {}
+   {:on-click click-fn
+    :class (when @selected-atom
+             "selected")}
    [:div.card-border-inset
     {}
     children]])
 
-(defn card [{:keys [title kind idx]} props]
-  (let [key (str idx kind title)]
+(defn handle-select [selected-atom card]
+  (swap! selected-atom not)
+  (swap! state/app-state assoc :selected-card card)
+  (js/console.log  @state/app-state))
+
+(defn card [{:keys [title kind idx]}]
+  (let [key (str idx kind title)
+        card {:title title
+              :kind kind
+              :idx idx}
+        selected-atom (r/atom false)]
     [card-border
+     (partial handle-select selected-atom card)
+     selected-atom
      [:h2 {:key (str key "idx")}
       idx]
      [:h1 {:key (str key "title")}
@@ -18,7 +33,8 @@
       kind]]))
 
 (defn card-back []
-  [card-border])
+  [:div.card-border {}
+   [:div.card-border-inset {}]])
 
 (defn slot []
   [:div.slot {}])
